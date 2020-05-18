ebb9fd31d9ab986d41b3a6cebcd3c59cb288bfad
yulinzhihou <yulinzhihou@gmail.com>
2020-05-18 16:14:54 +0800

    5-18 优化配置env

--- gs/global/set_ini.sh
+++ gs/global/set_ini.sh
@@ -4,6 +4,6 @@
 # date: 2020-05-17
 # comment: 根据env文件的环境变量，修改对应的配置文件，复制配置文件替换到指定目录，并给与相应权限
 #\cp -rf /etc/gs_ini/*.ini /tlgame/tlbb/Server/Config && chmod -R 777 /tlgame && chown -R root:root /tlgame
-if [ -n $TBLLDB_MYSQL_PASS ]; then
+if [ -n $TLBBDB_MYSQL_PASS ]; then
     tar zxf /.tlgame/gs/scripts/ini.tar.gz -C /tlgame/tlbb/Server/Config && chmod -R 777 /tlgame && chown -R root:root /tlgame
 fi
