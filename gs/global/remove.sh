67fee7f72abecf826fc6c13dc0ac921cbd520c75
yulinzhihou <yulinzhihou@gmail.com>
2020-05-20 15:17:09 +0800

    5-20 修复setini替换命令的问题-2

--- gs/global/remove.sh
+++ gs/global/remove.sh
@@ -3,4 +3,4 @@
 # mail: yulinzhihou@gmail.com
 # date: 2020-05-17
 # comment: 删除所有数据
-docker stop $(docker ps -a -q) && docker rm $(docker ps -a -q) && docker rmi $(docker images -q)
+docker stop $(docker ps -a -q) && docker rm $(docker ps -a -q) && docker rmi $(docker images -q) && rm -rf /tlgame
