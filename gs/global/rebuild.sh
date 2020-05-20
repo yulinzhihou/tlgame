97449c4f558c7f50476e56bbbf0791891535ba2c
yulinzhihou <yulinzhihou@gmail.com>
2020-05-20 15:16:01 +0800

    5-20 修复setini替换命令的问题-1

--- gs/global/rebuild.sh
+++ gs/global/rebuild.sh
@@ -3,4 +3,4 @@
 # mail: yulinzhihou@gmail.com
 # date: 2020-05-17
 # comment: 服务器环境重构，删除容器，重新启动
-docker stop $(docker ps -a -q) && docker rm $(docker ps -a -q) && rm -rf /tlgame/tlbb && cd ~/.tlgame/gs && docker-compose up -d
+docker stop $(docker ps -a -q) && docker rm $(docker ps -a -q) && rm -rf /tlgame/tlbb/* && cd ~/.tlgame/gs && docker-compose up -d
