f24b3f89a19cded6c8eb330cde4a5b14361491e2
yulinzhihou <yulinzhihou@gmail.com>
2020-07-02 15:58:20 +0800

    7-2 fix readme, swap command

--- gs/global/swap.sh
+++ gs/global/swap.sh
@@ -5,4 +5,5 @@
 # comment: 内存小，但是容量有20个G以上的云服务器。需要拓展虚拟内存
 mkdir -p /usr/swap && dd if=/dev/zero of=/usr/swap/swapfile bs=100M count=40 && \
 mkswap /usr/swap/swapfile && \
-chmod -R 600 /usr/swap/swapfile && swapon /usr/swap/swapfile
+chmod -R 600 /usr/swap/swapfile && swapon /usr/swap/swapfile && \
+echo "/usr/swap/swapfile swap swap defaults 0 0" >> /etc/fstab
