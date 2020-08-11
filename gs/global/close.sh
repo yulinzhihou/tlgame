831aa960d97e96898f48769f996916822694dc0a
yulinzhihou <yulinzhihou@163.com>
2020-08-11 22:28:39 +0800

    8-12 修改启动验证命令

--- gs/global/close.sh
+++ gs/global/close.sh
@@ -4,7 +4,8 @@
 # date: 2020-05-27
 # comment: 一键命令关闭所有
 cd ~/.tlgame/gs && \
-docker-compose exec -d server /bin/bash stop.sh
+docker-compose exec -d server /bin/bash stop.sh && \
+docker-compose exec -d server /home/billing/billing stop
 if [ $? == 0 ]; then
     echo "服务端已经关闭成功，如果需要重新开启，请运行，runtlbb 命令"
 fi
--- gs/global/runtlbb.sh
+++ gs/global/runtlbb.sh
@@ -4,7 +4,7 @@
 # date: 2020-05-17
 # comment: 一键开服，适合于那种可以一键开启的服务端，如果3-5分钟后，服务端没开启，则需要使用分步开服方式
 cd ~/.tlgame/gs && \
-docker-compose exec -d server /home/billing/billing \& && \
+docker-compose exec -d server /home/billing/billing up -d  && \
 sed -i 's/exit$/sleep 99999999/g' /tlgame/tlbb/run.sh && \
 cd ~/.tlgame/gs && \
 docker-compose exec -d server /bin/bash run.sh
