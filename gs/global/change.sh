d1525057672e4569906633061c2deab298775d03
yulinzhihou <yulinzhihou@163.com>
2020-08-15 18:56:54 +0800

    8-15 优化换端change命令

--- gs/global/change.sh
+++ gs/global/change.sh
@@ -11,4 +11,4 @@ cd ~/.tlgame/gs && \
 docker-compose up -d && \
 setini && \
 runtlbb
-echo -e "\e[44m 换端成功，请耐心等待几分钟后，建议使用：【link】再使用【top】 查看运行情况，或者使用备用命令【runtop】先进行查看开服的情况！\e[0m"
+echo -e "\e[44m 换端成功，请耐心等待几分钟后，建议使用：【runtop】查看开服的情况！\e[0m"
--- gs/global/resetini.sh
+++ gs/global/resetini.sh
@@ -3,12 +3,12 @@
 # mail: yulinzhihou@gmail.com
 # date: 2020-05-26
 # comment: 当用户需要重新生成数据库端口，密码时，则使用此命令进行重装写入配置，注意，执行完成后需要重启服务器再进行配置。否则需要使用 source /etc/profile.d 让数据临时生效
-echo -e "export WEB_MYSQL_PORT=`[ ! -z ${WEB_MYSQL_PORT} ] && echo ${WEB_MYSQL_PORT} || echo 33060`" > /tmp/pass.txt
-echo -e "export TLBB_MYSQL_PORT=`[ ! -z ${TLBB_MYSQL_PORT} ] && echo ${TLBB_MYSQL_PORT} || echo 33061`" >> /tmp/pass.txt
-echo -e "export LOGIN_PORT=`[ ! -z ${LOGIN_PORT} ] && echo ${LOGIN_PORT} || echo 13580`" >> /tmp/pass.txt
-echo -e "export SERVER_PORT=`[ ! -z ${SERVER_PORT} ] && echo ${SERVER_PORT} || echo 15680`" >> /tmp/pass.txt
-echo -e "export WEBDB_PASSWORD=`[ ! -z ${WEBDB_PASSWORD} ] && echo ${WEBDB_PASSWORD} || echo 123456`" >> /tmp/pass.txt
-echo -e "export TLBBDB_PASSWORD=`[ ! -z ${TLBBDB_PASSWORD} ] && echo ${TLBBDB_PASSWORD} || echo 123456`" >> /tmp/pass.txt
-echo -e "export BILLING_PORT=`[ ! -z ${BILLING_PORT} ] && echo ${BILLING_PORT} || echo 21818`" >> /tmp/pass.txt
-cat /tmp/pass.txt > /etc/profile.d/gsconfig.sh && rm -rf /tmp/pass.txt
+#echo -e "export WEB_MYSQL_PORT=`[ ! -z ${WEB_MYSQL_PORT} ] && echo ${WEB_MYSQL_PORT} || echo 33060`" > /tmp/pass.txt
+#echo -e "export TLBB_MYSQL_PORT=`[ ! -z ${TLBB_MYSQL_PORT} ] && echo ${TLBB_MYSQL_PORT} || echo 33061`" >> /tmp/pass.txt
+#echo -e "export LOGIN_PORT=`[ ! -z ${LOGIN_PORT} ] && echo ${LOGIN_PORT} || echo 13580`" >> /tmp/pass.txt
+#echo -e "export SERVER_PORT=`[ ! -z ${SERVER_PORT} ] && echo ${SERVER_PORT} || echo 15680`" >> /tmp/pass.txt
+#echo -e "export WEBDB_PASSWORD=`[ ! -z ${WEBDB_PASSWORD} ] && echo ${WEBDB_PASSWORD} || echo 123456`" >> /tmp/pass.txt
+#echo -e "export TLBBDB_PASSWORD=`[ ! -z ${TLBBDB_PASSWORD} ] && echo ${TLBBDB_PASSWORD} || echo 123456`" >> /tmp/pass.txt
+#echo -e "export BILLING_PORT=`[ ! -z ${BILLING_PORT} ] && echo ${BILLING_PORT} || echo 21818`" >> /tmp/pass.txt
+#cat /tmp/pass.txt > /etc/profile.d/gsconfig.sh && rm -rf /tmp/pass.txt
 echo -e "\e[44m 此命令暂时未开放，暂时不支持换配置文件 \e[0m"
