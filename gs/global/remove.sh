efdd683aa12d95ee408315086deadb58ede7ea65
yulinzhihou <yulinzhihou@gmail.com>
2020-12-19 23:43:31 +0800

    12-19 增加配置文件删除功能改成备份改名

--- gs/global/remove.sh
+++ gs/global/remove.sh
@@ -3,5 +3,9 @@
 # mail: yulinzhihou@gmail.com
 # date: 2020-05-17
 # comment: 删除所有数据
-docker stop $(docker ps -a -q) && docker rm $(docker ps -a -q) && docker rmi $(docker images -q) && rm -rf /tlgame && rm -rf ~/.tlgame
+docker stop $(docker ps -a -q) && \
+docker rm $(docker ps -a -q) && \
+docker rmi $(docker images -q) && \
+mv /tlgame  /tlgame-`date +%Y%m%d%H%I%S` && \
+rm -rf ~/.tlgame
 echo -e "\e[44m 数据清除成功，请重新安排文档教程命令第一步开始安装环境！！！ \e[0m"
