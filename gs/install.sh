ace5a07599d61b0401b221ae7e80d817b34529e1
yulinzhihou <yulinzhihou@gmail.com>
2021-01-08 20:20:53 +0800

    1-8 分解安装脚本

--- gs/install.sh
+++ gs/install.sh
@@ -10,40 +10,9 @@ export PATH
 cur_dir=$(cd -P -- "$(dirname -- "$0")" && pwd -P)
 startTime=`date +%s`
 
-# 获取系统位数，32 位，64 位
-Get_OS_Bit()
-{
-    if [[ `getconf WORD_BIT` = '32' && `getconf LONG_BIT` = '64' ]] ; then
-        Is_64bit='y'
-    else
-        Is_64bit='n'
-    fi
-}
+. scripts/main.sh
 
-# 获取平台名称
-Get_Dist_Version()
-{
-    if command -v lsb_release >/dev/null 2>&1; then
-        DISTRO_Version=$(lsb_release -sr)
-    elif [ -f /etc/lsb-release ]; then
-        . /etc/lsb-release
-        DISTRO_Version="$DISTRIB_RELEASE"
-    elif [ -f /etc/os-release ]; then
-        . /etc/os-release
-        DISTRO_Version="$VERSION_ID"
-    fi
-    if [[ "${DISTRO}" = "" || "${DISTRO_Version}" = "" ]]; then
-        if command -v python2 >/dev/null 2>&1; then
-            DISTRO_Version=$(python2 -c 'import platform; print platform.linux_distribution()[1]')
-        elif command -v python3 >/dev/null 2>&1; then
-            DISTRO_Version=$(python3 -c 'import platform; print(platform.linux_distribution()[1])')
-        else
-            Install_LSB
-            DISTRO_Version=`lsb_release -rs`
-        fi
-    fi
-    printf -v "${DISTRO}_Version" '%s' "${DISTRO_Version}"
-}
+Get_Dist_Name
 
 # 字体输出颜色
 Color_Text()
@@ -85,69 +54,6 @@ Disable_Selinux()
 {
     if [ -s /etc/selinux/config ]; then
         sed -i 's/^SELINUX=.*/SELINUX=disabled/g' /etc/selinux/config
-    fi
-}
-
-# 获取系统安装包组件名称
-Get_Dist_Name()
-{
-    if grep -Eqi "CentOS" /etc/issue || grep -Eq "CentOS" /etc/*-release; then
-        DISTRO='CentOS'
-        PM='yum'
-    elif grep -Eqi "Red Hat Enterprise Linux" /etc/issue || grep -Eq "Red Hat Enterprise Linux" /etc/*-release; then
-        DISTRO='RHEL'
-        PM='yum'
-    elif grep -Eqi "Aliyun" /etc/issue || grep -Eq "Aliyun" /etc/*-release; then
-        DISTRO='Aliyun'
-        PM='yum'
-    elif grep -Eqi "Fedora" /etc/issue || grep -Eq "Fedora" /etc/*-release; then
-        DISTRO='Fedora'
-        PM='yum'
-    elif grep -Eqi "Amazon Linux" /etc/issue || grep -Eq "Amazon Linux" /etc/*-release; then
-        DISTRO='Amazon'
-        PM='yum'
-    elif grep -Eqi "Debian" /etc/issue || grep -Eq "Debian" /etc/*-release; then
-        DISTRO='Debian'
-        PM='apt'
-    elif grep -Eqi "Ubuntu" /etc/issue || grep -Eq "Ubuntu" /etc/*-release; then
-        DISTRO='Ubuntu'
-        PM='apt'
-    elif grep -Eqi "Raspbian" /etc/issue || grep -Eq "Raspbian" /etc/*-release; then
-        DISTRO='Raspbian'
-        PM='apt'
-    elif grep -Eqi "Deepin" /etc/issue || grep -Eq "Deepin" /etc/*-release; then
-        DISTRO='Deepin'
-        PM='apt'
-    elif grep -Eqi "Mint" /etc/issue || grep -Eq "Mint" /etc/*-release; then
-        DISTRO='Mint'
-        PM='apt'
-    elif grep -Eqi "Kali" /etc/issue || grep -Eq "Kali" /etc/*-release; then
-        DISTRO='Kali'
-        PM='apt'
-    else
-        DISTRO='unknow'
-    fi
-    Get_OS_Bit
-}
-
-# 获取红帽版本
-Get_RHEL_Version()
-{
-    Get_Dist_Name
-    if [ "${DISTRO}" = "RHEL" ]; then
-        if grep -Eqi "release 5." /etc/redhat-release; then
-            echo "Current Version: RHEL Ver 5"
-            RHEL_Ver='5'
-        elif grep -Eqi "release 6." /etc/redhat-release; then
-            echo "Current Version: RHEL Ver 6"
-            RHEL_Ver='6'
-        elif grep -Eqi "release 7." /etc/redhat-release; then
-            echo "Current Version: RHEL Ver 7"
-            RHEL_Ver='7'
-        elif grep -Eqi "release 8." /etc/redhat-release; then
-            echo "Current Version: RHEL Ver 8"
-            RHEL_Ver='8'
-        fi
     fi
 }
 
@@ -272,11 +178,10 @@ CentOS_RemoveAMP()
     yum clean all
 }
 # 执行操作
-Disable_Selinux
-Set_Timezone
-Get_Dist_Name
 Get_Dist_Version
 RHEL_Modify_Source
+Disable_Selinux
+Set_Timezone
 update_install_plugins
 docker_ce
 set_command
---
+++ gs/scripts/main.sh
@@ -0,0 +1,101 @@
+#!/usr/bin/env bash
+# author: yulinzhihou
+# mail: yulinzhihou@gmail.com
+# date: 2021-01-08
+# 项目地址：https://github.com/yulinzhihou/tlgame.git
+# 项目地址：https://gitee.com/yulinzhihou/tlgame.git
+# 如果觉得写得有点点用，请麻烦点个 star
+
+Get_Dist_Version()
+{
+    if command -v lsb_release >/dev/null 2>&1; then
+        DISTRO_Version=$(lsb_release -sr)
+    elif [ -f /etc/lsb-release ]; then
+        . /etc/lsb-release
+        DISTRO_Version="$DISTRIB_RELEASE"
+    elif [ -f /etc/os-release ]; then
+        . /etc/os-release
+        DISTRO_Version="$VERSION_ID"
+    fi
+    if [[ "${DISTRO}" = "" || "${DISTRO_Version}" = "" ]]; then
+        if command -v python2 >/dev/null 2>&1; then
+            DISTRO_Version=$(python2 -c 'import platform; print platform.linux_distribution()[1]')
+        elif command -v python3 >/dev/null 2>&1; then
+            DISTRO_Version=$(python3 -c 'import platform; print(platform.linux_distribution()[1])')
+        else
+            Install_LSB
+            DISTRO_Version=`lsb_release -rs`
+        fi
+    fi
+    printf -v "${DISTRO}_Version" '%s' "${DISTRO_Version}"
+}
+
+Get_Dist_Name()
+{
+    if grep -Eqi "CentOS" /etc/issue || grep -Eq "CentOS" /etc/*-release; then
+        DISTRO='CentOS'
+        PM='yum'
+    elif grep -Eqi "Red Hat Enterprise Linux" /etc/issue || grep -Eq "Red Hat Enterprise Linux" /etc/*-release; then
+        DISTRO='RHEL'
+        PM='yum'
+    elif grep -Eqi "Aliyun" /etc/issue || grep -Eq "Aliyun" /etc/*-release; then
+        DISTRO='Aliyun'
+        PM='yum'
+    elif grep -Eqi "Fedora" /etc/issue || grep -Eq "Fedora" /etc/*-release; then
+        DISTRO='Fedora'
+        PM='yum'
+    elif grep -Eqi "Amazon Linux" /etc/issue || grep -Eq "Amazon Linux" /etc/*-release; then
+        DISTRO='Amazon'
+        PM='yum'
+    elif grep -Eqi "Debian" /etc/issue || grep -Eq "Debian" /etc/*-release; then
+        DISTRO='Debian'
+        PM='apt'
+    elif grep -Eqi "Ubuntu" /etc/issue || grep -Eq "Ubuntu" /etc/*-release; then
+        DISTRO='Ubuntu'
+        PM='apt'
+    elif grep -Eqi "Raspbian" /etc/issue || grep -Eq "Raspbian" /etc/*-release; then
+        DISTRO='Raspbian'
+        PM='apt'
+    elif grep -Eqi "Deepin" /etc/issue || grep -Eq "Deepin" /etc/*-release; then
+        DISTRO='Deepin'
+        PM='apt'
+    elif grep -Eqi "Mint" /etc/issue || grep -Eq "Mint" /etc/*-release; then
+        DISTRO='Mint'
+        PM='apt'
+    elif grep -Eqi "Kali" /etc/issue || grep -Eq "Kali" /etc/*-release; then
+        DISTRO='Kali'
+        PM='apt'
+    else
+        DISTRO='unknow'
+    fi
+    Get_OS_Bit
+}
+
+Get_RHEL_Version()
+{
+    Get_Dist_Name
+    if [ "${DISTRO}" = "RHEL" ]; then
+        if grep -Eqi "release 5." /etc/redhat-release; then
+            echo "Current Version: RHEL Ver 5"
+            RHEL_Ver='5'
+        elif grep -Eqi "release 6." /etc/redhat-release; then
+            echo "Current Version: RHEL Ver 6"
+            RHEL_Ver='6'
+        elif grep -Eqi "release 7." /etc/redhat-release; then
+            echo "Current Version: RHEL Ver 7"
+            RHEL_Ver='7'
+        elif grep -Eqi "release 8." /etc/redhat-release; then
+            echo "Current Version: RHEL Ver 8"
+            RHEL_Ver='8'
+        fi
+    fi
+}
+
+Get_OS_Bit()
+{
+    if [[ `getconf WORD_BIT` = '32' && `getconf LONG_BIT` = '64' ]] ; then
+        Is_64bit='y'
+    else
+        Is_64bit='n'
+    fi
+}
